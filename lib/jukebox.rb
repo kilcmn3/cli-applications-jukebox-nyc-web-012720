c32d2c1f12b864b909e2fa2a712f862d0c2cb5d6
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-18 09:24:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,14 +21,14 @@ def help
 end
 
 def play(songs)
-  # puts "Please enter a song name or number:"
-  # a = gets.strip
-  #   if a.to_i < 10
-  #       puts "Playing #{songs}"
-  #     elsif a
-  #       puts "Invalid input, please try again"
-  #       end
-  print songs
+  # # puts "Please enter a song name or number:"
+  # # a = gets.strip
+  # #   if a.to_i < 10
+  # #       puts "Playing #{songs}"
+  # #     elsif a
+  # #       puts "Invalid input, please try again"
+  # #       end
+  # print songs
 end
 
 def list(songs)
