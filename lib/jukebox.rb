a18b685e89b2653330453ccae1bde87650966009
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-18 09:37:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,12 +23,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   a = gets.strip
-
-      if list(a) == a
-        puts "Playing #{b}"
-      else
-        puts "Invalid input, please try again"
-        end
+  songs.each_with_index do |item, index|
+  end
       end
 
 def list(song)
