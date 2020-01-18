d4b7923b18c17f6f7bf137143277dd4c9beaa995
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-18 09:40:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,8 @@ def play(songs)
   songs.each_with_index do |item, index|
     if a.to_i == index + 1 || a == item
       puts "#{item}"
-    elsif a == index
+    else
+      puts "Invalid input, please try again"
     end
   end
 end
