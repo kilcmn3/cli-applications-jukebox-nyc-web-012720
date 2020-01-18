1175a5a52f066f779fca1de504b41e1550bf1eff
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-18 09:45:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,7 +23,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   a = gets.strip
-  songs.each do |item, index|
+  songs.each_with_index do |item, index|
+
 end
 
 def list(song)
